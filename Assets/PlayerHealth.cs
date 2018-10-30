5b9c56a461d4a050b57af4376cfaba93880e5dce
Eduardo Simón Picón <al341513@uji.es>
2018-10-30 18:19:18 +0100

    Añadido dotween y pantalla roja al dañarte

--- Assets/PlayerHealth.cs
+++ Assets/PlayerHealth.cs
@@ -1,22 +1,31 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using DG.Tweening;
 using UnityEngine;
+using UnityEngine.Experimental.UIElements;
 using UnityEngine.SceneManagement;
 
 public class PlayerHealth : MonoBehaviour {
 	
 	public float MaxHeatlh;
+	public UnityEngine.UI.Image DamagedImage;
 	
 	private float health;
+	private Sequence hurtImageSequence;
 
 	private void Start()
 	{
 		health = MaxHeatlh;
+		DamagedImage.DOFade(0.0f,0.01f);
 	}
 
 	public void DecreaseHealth(float amount)
 	{
 		health -= amount;
+		hurtImageSequence = DOTween.Sequence();
+		hurtImageSequence.Append(DamagedImage.DOFade(1.0f,0.1f));
+		hurtImageSequence.Append(DamagedImage.DOFade(0.0f,0.7f));
+		hurtImageSequence.Play();
 		
 		if(health <= 0)
 			SceneManager.LoadScene("Menu");
