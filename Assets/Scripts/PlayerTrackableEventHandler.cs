42cb44cbfe35b7b7cd0f68fe85299ab580c9f29c
Chepriav <al341533@uji.es>
2018-11-01 22:29:47 +0100

    cambio nombre playerEventHAndler

--- Assets/Scripts/PlayerTrackableEventHandler.cs
+++ Assets/Scripts/PlayerTrackableEventHandler.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using Vuforia;
 
-public class SpawnerController : DefaultTrackableEventHandler
+public class PlayerTrackableEventHandler : DefaultTrackableEventHandler
 {
 
 	public GameObject spawner;
